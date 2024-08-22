027e143b51285063dbb8836570f6601c9fc54fba
Pablo Velarde <pablovelarde905@gmail.com>
2024-08-21 23:23:57 -0400

    Created new modern nav bar

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,28 +1,27 @@
+import Logo from '../assets/Logo.png';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center py-4">
-      <div className="flex items-center">
-        <p className="text-lg font-bold text-gray-800">
-          ResumeAI
-        </p>
-      </div>
-      <ul className="flex items-center justify-end space-x-12 mr-8">
-        <li>
-            <Link to="/" className="text-gray-600 hover:text-gray-900">Home</Link>
+    <nav className="flex justify-between items-center py-4 px-6 bg-white shadow-md">
+      <img src={Logo} alt="Logo" className="h-14 mr-1 object-fit-contain" />
+      <Link to="/" className="text-3xl font-bold text-transparent bg-gradient-to-r from-blue-500 to-indigo-500 bg-clip-text">ResumeAI</Link>
+      <ul className="flex items-center ml-4">
+        <li className="mr-4">
+          <Link to="/about" className="text-gray-600 hover:text-blue-900 transition duration-300 ease-in-out">About</Link>
         </li>
-        <li>
-            <Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link>
+        <li className="mr-4">
+          <Link to="/contact" className="text-gray-600 hover:text-blue-900 transition duration-300 ease-in-out">Contact</Link>
         </li>
-        <li>
-            <Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link>
-        </li>
-        <li>
-            <Link to="/login" className="text-gray-600 hover:text-gray-900">Login</Link>
+        <li className="mr-4">
+          <Link to="/pricing" className="text-gray-600 hover:text-blue-900 transition duration-300 ease-in-out">Pricing</Link>
         </li>
       </ul>
+      <div className="ml-auto flex items-center">
+        <Link to="/login" className="text-gray-600 hover:text-gray-900 transition duration-300 ease-in-out mr-4">Log In</Link>
+        <Link to="/sign-up" className="bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-blue-700 hover:to-indigo-700 text-white font-bold py-2 px-4 rounded">Sign Up</Link>
+      </div>
     </nav>
   );
 };
