1e8399ffface47c13f95a879888fd97a11fa618b
Pablo Velarde <pablovelarde905@gmail.com>
2024-09-06 22:05:12 -0400

    Refactored code

--- server/config.py
+++ server/config.py
@@ -3,10 +3,14 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 
+# Create the Flask app
 app = Flask(__name__)
+
+# Enable CORS
 cors = CORS(app, origins='http://localhost:5173')
 bcrypt = Bcrypt(app)
 
+# Connect to the MongoDB
 client = MongoClient('localhost', 27017)
 
 db = client.flask_database
